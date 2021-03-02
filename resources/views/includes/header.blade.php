f06dc2a841e6aaa13f14da5643abf6bfb1465959
Андрей Попов <A.kolesnikov@a-b63.ru>
2021-03-02 21:08:11 +0400

    learn includes yield if

---
+++ resources/views/includes/header.blade.php
@@ -0,0 +1,24 @@
+<header>
+	<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+		<div class="container d-flex align-items-center justify-content-center">
+			<a class="navbar-brand" href="/">Unity-потуги</a>
+
+			<div class="collapse navbar-collapse d-flex align-items-center justify-content-between" id="navbarCollapse">
+				<ul class="navbar-nav me-auto mb-2 mb-md-0">
+					<li class="nav-item">
+						<a class="nav-link" href="/about">О нас</a>
+					</li>
+
+					<li class="nav-item">
+						<a class="nav-link" href="/contact" tabindex="-1" aria-disabled="true">Контакты</a>
+					</li>
+				</ul>
+
+				<form class="d-flex">
+					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+					<button class="btn btn-outline-success" type="submit">Search</button>
+				</form>
+			</div>
+		</div>
+	</nav>
+</header>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,11 +4,31 @@
    <meta charset="UTF-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+   <link rel="stylesheet" href="/css/app.css">
+
    <title>@yield('title')</title>
 </head>
 <body>
-   @yield('content')
+   @include('includes.header')
 
-   @include('includes.aside')
+   @if(Request::is('/'))
+      <h1 style="padding-top: 100px;">Ты пидор</h1>
+   @endif
+
+   <div class="content">
+      <div class="container">
+         <div class="row">
+            <div class="col-8">
+               @yield('content')
+            </div>
+
+            <div class="col-4">
+               @include('includes.aside')
+            </div>
+         </div>
+      </div>
+   </div>
 </body>
 </html>
