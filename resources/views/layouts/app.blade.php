bbce021360bc5d5c8a956988ab5a796207523ba9
Андрей Попов <A.kolesnikov@a-b63.ru>
2021-03-07 11:59:21 +0400

    nochange

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,34 +1,34 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-   <meta charset="UTF-8">
-   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-   <link rel="stylesheet" href="/css/app.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<link rel="stylesheet" href="/css/app.css">
 
-   <title>@yield('title')</title>
+<title>@yield('title')</title>
 </head>
 <body>
-   @include('includes.header')
+	@include('includes.header')
 
-   @if(Request::is('/'))
-      <h1 style="padding-top: 100px;">Ты пидор</h1>
-   @endif
+	@if(Request::is('/'))
+		<!-- <h1 style="padding-top: 100px;">Ты пидор</h1> -->
+	@endif
 
-   <div class="content">
-      <div class="container">
-         <div class="row">
-            <div class="col-8">
-               @yield('content')
-            </div>
+	<div class="content">
+	<div class="container">
+		<div class="row">
+			<div class="col-8">
+				@yield('content')
+			</div>
 
-            <div class="col-4">
-               @include('includes.aside')
-            </div>
-         </div>
-      </div>
-   </div>
+			<div class="col-4">
+				@include('includes.aside')
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
